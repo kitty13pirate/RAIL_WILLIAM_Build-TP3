bbfc26fe25a2b0ce929e6c307c620661eb080429
kitty13pirate <williamrail27@hotmail.fr>
2021-04-08 10:52:11 -0400

    Version 1 animation personnage finie
    Animations du personnage sont fonctionnels, la camera suit le joueur mais le joueur ne se tourne pas encore.

---
+++ Assets/Scripts/PlayerCharacterBoi.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterBoi : MonoBehaviour
+{
+    // La vitesse du personnage
+    public float movementSpeed;
+
+    // Pour verifier les inputs
+    private float inputVertical;
+    private float inputHorizontal;
+
+    // La direction du mouvement
+    private Vector3 moveDirection;
+
+    //Le rigidbody
+    private Rigidbody rb;
+
+    // L'animator
+    private Animator playerCharacterBoiAnimator;
+
+    // Pour verifier si le personnage utilise un sort
+    private bool isCasting;
+
+    // Pour verifier le cooldown des habilites
+    private bool pushCooldown = false;
+    private bool healCooldown = false;
+    private bool earthCooldown = false;
+    private bool fireCooldown = false;
+    private bool waterCooldown = false;
+    private bool windCooldown = false;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerCharacterBoiAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Fair regarder au personnage la position de la souris
+
+        // Vérifier les inputs du joueur
+        // Vertical (W, S et Joystick avant/arrière)
+        inputVertical = Input.GetAxis("Vertical");
+        // Horizontal (A, D et Joystick gauche/droite)
+        inputHorizontal = Input.GetAxis("Horizontal");
+
+        //Les variables pour les animations
+        playerCharacterBoiAnimator.SetFloat("Horizontal", inputHorizontal);
+        playerCharacterBoiAnimator.SetFloat("Vertical", inputVertical);
+
+        // Vecteur de mouvements (Avant/arrière + Gauche/Droite)
+        moveDirection = transform.forward * inputVertical + transform.right * inputHorizontal;
+
+        // Les touches pour les sorts
+        if (Input.GetMouseButtonDown(0))
+        {
+            playerCharacterBoiAnimator.SetTrigger("Basic Spell");
+        }
+        if (Input.GetMouseButtonDown(1) && !pushCooldown)
+        {
+            playerCharacterBoiAnimator.SetTrigger("Push Spell");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            playerCharacterBoiAnimator.SetTrigger("Fire Spell");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            playerCharacterBoiAnimator.SetTrigger("Earth Spell");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            playerCharacterBoiAnimator.SetTrigger("Water Spell");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            playerCharacterBoiAnimator.SetTrigger("Wind Spell");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            playerCharacterBoiAnimator.SetTrigger("Heal Spell");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Déplacer le personnage selon le vecteur de direction
+        rb.MovePosition(rb.position + moveDirection.normalized * movementSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/TopdownCameraPositioner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopdownCameraPositioner : MonoBehaviour
+{
+    public Transform playerCharacter;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(playerCharacter.transform.position.x, playerCharacter.transform.position.y + 3f, playerCharacter.transform.position.z - 2f);
+        transform.LookAt(playerCharacter);
+    }
+}
